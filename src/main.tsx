e03691bf84f8d2685e034ecec576334df99998dc
Madeline Yi <sunhunfunbun@gmail.com>
2023-07-04 13:17:42 -0500

    adjusted files to properly deploy gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import App from "./App";
 import AboutPage from "./components/AboutPage";
 import ProjectsPage from "./components/ProjectsPage";
 import GalleryPage from "./components/GalleryPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
