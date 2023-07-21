5b83c99ad8174353416602f9270408b192f05164
Madeline Yi <sunhunfunbun@gmail.com>
2023-07-21 18:40:52 -0500

    added cat facts to make pages more interesting

--- src/components/WorkInProgress.tsx
+++ src/components/WorkInProgress.tsx
@@ -1,10 +1,37 @@
 import "../style/WorkInProgress.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function WorkInProgress() {
+  const [facts, setFacts] = useState([]);
+  const [loading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // fetching cat fact
+    axios
+      .get("https://catfact.ninja/fact")
+      .then((response: any) => {
+        setFacts(response.data.fact);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="entire-container">
       <div className="text">
-        Oops, this page is currently in progress! Sorry for the inconvenience.
+        <div className="title">
+          Oops, this page is currently in progress! Sorry for the inconvenience.
+          <br />
+        </div>
+        <div className="body">
+          <div className="emphasis">
+            Here is a random cat fact to keep you entertained!{" "}
+          </div>
+          <br />
+          <div className="fact"> {loading ? "Getting cat fact" : facts} </div>
+        </div>
       </div>
 
       <img
